d5a71ad22f8a504e0cef98f1fb5bd6b60b666324
Phat Huynh <phathuynhwork18@gmail.com>
2025-05-21 21:56:02 +0700

    initial commit for deploy frontend

---
+++ apps/main.py
@@ -0,0 +1,6 @@
+import streamlit as st
+
+st.write(
+    "<h2> <b style='color:red; text-align: center; font-size: 30px;'> Predict industrial maintenance   </b> </h2>",
+    unsafe_allow_html=True,
+)
